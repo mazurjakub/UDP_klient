174d96178cedc4a42ef6a8472067be8bbe80d036
mazurjakub <71710141+mazurjakub@users.noreply.github.com>
2022-03-12 16:00:04 +0100

    Přidejte soubory projektu.

---
+++ UDP_klient/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_klient
+{
+    class Program
+    {
+        // From http://stackoverflow.com/questions/6803073/get-local-ip-address
+        public string GetLocalIp()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Failed to get local IP");
+        }
+
+        public string GetExternalIp()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                string res = GetExternalIpWithTimeout(400);
+                if (res != "")
+                {
+                    return res;
+                }
+            }
+            throw new Exception("Failed to get external IP");
+        }
+        private static string GetExternalIpWithTimeout(int timeoutMillis)
+        {
+            string[] sites = new string[] {
+                "http://ipinfo.io/ip",
+                "http://icanhazip.com/",
+                "http://ipof.in/txt",
+                "http://ifconfig.me/ip",
+                "http://ipecho.net/plain"
+            };
+            foreach (string site in sites)
+            {
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(site);
+                    request.Timeout = timeoutMillis;
+                    using (var webResponse = (HttpWebResponse)request.GetResponse())
+                    {
+                        using (Stream responseStream = webResponse.GetResponseStream())
+                        {
+                            using(StreamReader responseReader = new StreamReader(responseStream, Encoding.UTF8)){
+                                return responseReader.ReadToEnd().Trim();
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+
+            return "";
+
+        }
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                byte[] receivedData;
+                int recv = 0;
+                string messageToSend = "Hello world";
+                int byteCount = Encoding.ASCII.GetByteCount(messageToSend);
+                byte[] sendData = Encoding.ASCII.GetBytes(messageToSend);
+
+
+                var client = new UdpClient();
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // endpoint where server is listening
+                client.Connect(ep);
+
+                // send data
+                client.Send(sendData,byteCount);
+
+                // then receive data
+                receivedData = client.Receive(ref ep);
+
+                foreach (byte b in receivedData)
+                {
+                    if (b != 0)
+                    {
+                        recv++;
+                    }
+                }
+
+                string request = Encoding.UTF8.GetString(receivedData, 0, recv);
+
+                Console.WriteLine("receive data from " + ep.ToString());
+                Console.WriteLine("Recieved data: " + request);
+
+                Console.Read();
+            }
+
+
+        }
+    }
+}
